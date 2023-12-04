6e9370e762a867a17b28f47c4190c3cb31551c15
Victor Smirnov <csatom@yandex.ru>
2023-12-04 20:46:15 +0300

    added match-score page

---
+++ handlers/match_score_post_handler.py
@@ -0,0 +1,9 @@
+from service.match_service import MatchService
+
+
+
+class MatchScorePostHandler:
+    """
+    Класс для обработки поступающих данных на странице '/match-score'
+    Обрабатывает нажатие на кнопки - “игрок 1 выиграл текущее очко”, “игрок 2 выиграл текущее очко”
+    """
