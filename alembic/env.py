ee8740ea5c2408243e8eecf3d22a53d4d143c1a2
Victor Smirnov <csatom@yandex.ru>
2023-12-13 20:53:58 +0300

    corr path in env.py alembic

--- alembic/env.py
+++ alembic/env.py
@@ -18,9 +18,12 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-from ..model import Base
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from model.database import Base
 target_metadata = Base.metadata
-from ..model import settings
+from model.config import settings
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
