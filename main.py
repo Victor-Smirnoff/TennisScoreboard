c6b117e932f575e08afa17cee3b6fdab339c4028
Victor Smirnov <csatom@yandex.ru>
2023-12-14 12:52:02 +0300

    return url

--- main.py
+++ main.py
@@ -23,9 +23,6 @@ class MainApp:
     def process_http_request(self, environ, start_response):
         status = "200 OK"
 
-        if "tennis/" in environ["PATH_INFO"]:
-            environ["PATH_INFO"] = environ["PATH_INFO"].replace("tennis/", "")
-
         if environ["PATH_INFO"] == "/" and environ["REQUEST_METHOD"] == "GET":
             handler = IndexHandler()
             HTML = handler()
